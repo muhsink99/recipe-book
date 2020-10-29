8a5e41a33ceb4cbc00c9977f646e3d48eac0b3b6
muhsink99 <muhsinkumbay99@gmail.com>
2020-10-29 15:46:36 +0000

    add boostrap styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Recipe book</h1>
-        <p>Keep track of your favourite recipes across the web</p>
-        <button>Sign up </button>
-        <button>Sign in </button>
-      </header>
+      <h1>Recipe book</h1>
+      <p>Keep track of your favourite recipes across the web</p>
+      <button>Sign up </button>
+      <button>Sign in </button>
     </div>
   );
 }
